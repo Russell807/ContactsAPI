0dc6ad09af06b0d81bc378044448a750c8768fa3
HP <HP@DESKTOP-DPHENB9>
2023-08-31 13:01:47 +0800

    Add project files.

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
